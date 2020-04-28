0dd0eab42f6d3b1e50367aa45f71ac0e7f4f5a96
Yuto Ono <stellarwindmaster@gmail.com>
2020-04-28 15:11:04 +0900

    環境設定ファイル

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 init:
-	cp wp-init.sh docs/
+	cp .env wp-init.sh docs
 	docker-compose exec wp chmod +x wp-init.sh
 	docker-compose exec wp bash ./wp-init.sh
 
--- wp-init.sh
+++ wp-init.sh
@@ -1,12 +1,15 @@
+# 設定ファイル読み込み
+. .env
+
 # wp-cli のインストール
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 # WordPress 初期設定
-wp core install --url="http://localhost:8000" --title="Title" \
-	--admin_user="user" --admin_password="password" \
-	--admin_email="example@example.com" --allow-root
+wp core install --url="http://localhost:$PORT" --title="$WP_TITLE" \
+	--admin_user="$WP_USER" --admin_password="$WP_PASSWORD" \
+	--admin_email="$WP_EMAIL" --allow-root
 wp plugin uninstall hello --allow-root
 wp plugin install all-in-one-wp-migration --activate --allow-root
 wp core language install ja --activate --allow-root
