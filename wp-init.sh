a7a90cea67f06a5d1041766e05c4c20bb8926b75
Yuto Ono <stellarwindmaster@gmail.com>
2020-05-01 11:39:03 +0900

    PHPファイルアップロードサイズの引き上げ

--- wp-init.sh
+++ wp-init.sh
@@ -1,3 +1,6 @@
+# エラーが発生したら終了する
+set -e
+
 # 設定ファイル読み込み
 . .env
 
@@ -14,3 +17,8 @@ wp plugin uninstall hello --allow-root
 wp plugin install all-in-one-wp-migration --activate --allow-root
 wp core language install ja --activate --allow-root
 wp plugin install wp-multibyte-patch --activate --allow-root
+
+# ファイルアップロード　上限引き上げ
+echo "php_value memory_limit 600M" >> .htaccess
+echo "php_value post_max_size 600M" >> .htaccess
+echo "php_value upload_max_filesize 512M" >> .htaccess
